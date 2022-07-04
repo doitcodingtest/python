236e2c2e7ccd711cb10382f52b47b9641355208d
doitcodingtest <108266421+doitcodingtest@users.noreply.github.com>
2022-07-05 02:23:57 +0900

    Update P2098_외판원순회.py

--- 동적계획법/P2098_외판원순회.py
+++ 동적계획법/P2098_외판원순회.py
@@ -3,27 +3,23 @@ input = sys.stdin.readline
 N = int(input())
 W = []
 for i in range(N):
-    W.append([])
-    W[i] = list(map(int, input().split()))
-
+ W.append([])
+ W[i] = list(map(int, input().split()))
 D = [[0 for j in range(1 << 16)] for i in range(16)]
-for i in range(N):
-    for j in range(0, 1 << N):
-        D[i][j] = sys.maxsize
 
-
-def tsp(c, v):
-    if v == (1 << N) - 1:
+def tsp(c,v):
+    if v == (1<<N)-1:
         if W[c][0] == 0:
-            return sys.maxsize
+            return float('inf')
         else:
             return W[c][0]
-    if D[c][v] != sys.maxsize:
+    if D[c][v] != 0:
         return D[c][v]
+    min_val = float('inf')
     for i in range(0, N):
-        if (v & (1 << i)) == 0 and W[c][i] != 0:
-            D[c][v] = min(D[c][v], tsp(i, (v | (1 << i))) + W[c][i])
+        if (v & (1<<i)) == 0 and W[c][i] != 0:
+            min_val = min(min_val, tsp(i, (v | (1 << i))) + W[c][i])
+    D[c][v] = min_val
     return D[c][v]
 
-
-print(tsp(0, 1))
+print(tsp(0,1))
