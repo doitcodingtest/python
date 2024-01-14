c15e948a685e631fe945907e373f06f1370bda5c
doitcodingtest <108266421+doitcodingtest@users.noreply.github.com>
2024-01-14 20:16:25 +0900

    Update P1874_스택수열.py

--- 자료구조/P1874_스택수열.py
+++ 자료구조/P1874_스택수열.py
@@ -1,30 +1,28 @@
-N = int(input())
+N = int(input());
 A = [0]*N
 for i in range(N):
-    A[i] = int(input())
+    A[i] = int(input());
 stack = []
 num = 1
 result = True
-answer = ""
+answer = []
 for i in range(N):
     su = A[i]
-    if su >= num:   #현재 수열 값 >= 오름차순 자연수: 값이 같아질 때까지 push() 수행
+    if su >= num:
         while su >= num:
             stack.append(num)
-            num += 1
-            answer += "+\n"
+            num+=1
+            answer.append('+')
         stack.pop()
-        answer += "-\n"
-    else:           #현재 수열 값 < 오름차순 자연수: pop()을 수행해 수열 원소를 꺼냄
+        answer.append('-')
+    else:
         n = stack.pop()
-        if n > su:  #스택의 가장 위의 수가 만들어야 하는 수열의 수보다 크면 수열을 출력할 수 없음
+        if n > su:
             print("NO")
             result = False
             break
         else:
-            answer += "-\n"
+            answer.append('-')
 if result:
-    print(answer)
-
-
-
+    for i in answer:
+        print(i)
